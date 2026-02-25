11dc22b96b5f57c815bd3cc5d1ce5d7c774d5392
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-02-25 10:42:06 +0000

    i added index method in admincontroller which return the view of the dashboard

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Colocation;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all()->where('role_id', '!=' , 1);
+        $collocations = Colocation::count();
+        $activeColls = Colocation::where('status', 'active')->count();
+
+        return view('dashboard', compact('users', 'collocations', 'activeColls'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function banUser($userID)
+    {
+        $user = User::find($userID);
+        $user->is_banned = 1;
+        $user->save();
+
+        return redirect()->route('Admin.show');
+    }
+    public function UnbanUser($userID)
+    {
+        $user = User::find($userID);
+        $user->is_banned = 0;
+        $user->save();
+        return redirect()->route('Admin.show');
+
+    }
+}
